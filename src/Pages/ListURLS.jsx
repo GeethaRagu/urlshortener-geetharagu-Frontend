7069fb22f4af924a1e33753725ee3f504c9b7a50
GeethaRagu <geethumsp@gmail.com>
2024-06-29 20:09:49 +0530

    updated

--- src/Pages/ListURLS.jsx
+++ src/Pages/ListURLS.jsx
@@ -21,45 +21,42 @@ const ListURLS = () => {
   };
   return (
     <section className="bg-primary py-3 py-md-5 py-xl-8">
-      <div className="container">
-        <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
-          <Table className="table-fixed">
-            <thead>
-              <tr>
-                <th className="w-15 text-pretty p-4">Original URL</th>
-                <th className="w-15 text-pretty p-4">Shorten URL</th>
-                <th className="w-10 text-pretty ">Clicks</th>
-              </tr>
-            </thead>
-            <tbody>
-              {urllist.map((ele, index) => {
-                return (
-                  <tr key={index} className="">
-                    <td className="w-15 text-pretty p-4 text-break">
-                      {ele.origUrl}
-                    </td>
-                    <td className="w-15 text-pretty p-4 ">
-                      <Link
-                        to={`${apiurl}/url/${ele.urlId}`}
-                        className="hover:underline"
-                      >
-                        {ele.shortUrl}
-                      </Link>
-                    </td>
-                    <td className="w-10 text-pretty">{ele.clicks}</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </Table>
-        </div>
-        <Link
-          to="/dashboard"
-          className="mt-4 pb-4  mx-2 hover:underline text-white"
-        >
-          Back to Dashboard
-        </Link>
+    <div className="container">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        <Table className="table-fixed">
+          <thead>
+            <tr>
+              <th className="w-15 text-pretty p-4">Original URL</th>
+              <th className="w-15 text-pretty p-4">Shorten URL</th>
+              <th className="w-10 text-pretty ">Clicks</th>
+            </tr>
+          </thead>
+          <tbody>
+            {urllist.map((ele, index) => {
+              return (
+                <tr key={index} className="">
+                  <td className="w-15 text-pretty p-4 text-break">
+                    {ele.origUrl}
+                  </td>
+                  <td className="w-15 text-pretty p-4 ">
+                    <Link
+                      to={`https://urlshortener-backend-yzob.onrender.com/api/url/${ele.urlId}`}
+                      className="hover:underline"
+                    >
+                      {ele.shortUrl}
+                    </Link>
+                  </td>
+                  <td className="w-10 text-pretty">{ele.clicks}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
       </div>
+      <Link to="/dashboard" className="mt-4 pb-4  mx-2 hover:underline text-white">
+        Back to Dashboard
+      </Link>
+    </div>
     </section>
   );
 };
